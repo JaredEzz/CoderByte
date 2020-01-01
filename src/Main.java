8b09848d15ebaff44237b490781003d28346571a
Jared Hasson <jhasson@simplifile.com>
2020-01-01 11:28:20 -0700

    backup update

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,82 @@
-import java.util.*;
-import java.io.*;
+import java.util.HashMap;
 
-class Main {
-    public static String AlphabetSoup(String str) {
+public class Main {
 
-        StringBuilder sb = new StringBuilder();
 
-        for (char c = 'a'; c < 'z'; c++) {
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == c) {
-                    sb.append(c);
+
+    // A TreeMap is a great data structure to implement this function
+    // with because it doesn't allow duplicate keys
+    static int duplicatCount(String s) {
+        int count = 0;
+        HashMap<Character, Integer> map = new HashMap();
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (map.containsKey(s.charAt(i))) {
+                if (map.get(s.charAt(i)) == 1) {
+                    count++;
+                    map.put(s.charAt(i), 2);
                 }
+            }
+            else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+        return count;
+    }
+
+    static int charToInt(String s) {
+        int colInt = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (i == (s.length() - 1)) {
+                int ascii = (int) s.charAt(i);
+                ascii = ascii - 64;
+                colInt = colInt + ascii;
+            }
+        else {
+                colInt = colInt + 26;
             }
         }
 
-        return sb.toString();
-
+        return colInt;
     }
 
-    public static void main (String[] args) {
-        // keep this function call here
-        Scanner s = new Scanner(System.in);
-        System.out.print(AlphabetSoup("coderbyte"));
+    static char winner(String s) {
+        char result = 'C';
+
+        if (s.charAt(0) == s.charAt(1) && s.charAt(0) == s.charAt(2)) {
+            return s.charAt(0);
+        }
+        else if (s.charAt(3) == s.charAt(4) && s.charAt(3) == s.charAt(4)) {
+            return s.charAt(3);
+        }
+        else if (s.charAt(6) == s.charAt(7) && s.charAt(6) == s.charAt(8)) {
+            return s.charAt(6);
+        }
+        else if (s.charAt(0) == s.charAt(3) && s.charAt(0) == s.charAt(6)) {
+            return s.charAt(0);
+        }
+        else if (s.charAt(1) == s.charAt(4) && s.charAt(7) == s.charAt(4)) {
+            return s.charAt(1);
+        }
+        else if (s.charAt(2) == s.charAt(5) && s.charAt(2) == s.charAt(8)) {
+            return s.charAt(2);
+        }
+        else if (s.charAt(0) == s.charAt(4) && s.charAt(0) == s.charAt(8)) {
+            return s.charAt(0);
+        }
+        else if (s.charAt(2) == s.charAt(4) && s.charAt(2) == s.charAt(6)) {
+            return s.charAt(2);
+        }
+
+        return result;
     }
 
+    public static void main(String[] args){
+        System.out.print(winner("A-OXAOXOA"));
+    }
 }
+
+
+
+// Thanks for watching through that!
